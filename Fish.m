7ccb006507980d96c45f9b59611ca83bfaf9b246
egillmilan <egill.milan@gmail.com>
2023-05-01 14:18:33 +0200

    1.2.b done

--- Fish.m
+++ Fish.m
@@ -1,13 +1,26 @@
 % EL2220 Modeling a fishing scenario
 clc; clear all; close all;
 
-x = (linspace(0,10,11)/10)*2000;    % [0, 200, 400, ..., 2000]
-frx_dp = [0, 50, 100, 200, 320, 500, 550, 480, 280, 130, 0];
+% ================ a. ================
+xr = (linspace(0,10,11)/10)*2000;       % [0, 200, 400, ..., 2000]
+fr_dp = [0, 50, 100, 200, 320, 500, 550, 480, 280, 130, 0];
 
-n = 7;                              % Degree of fitted polynomial
-frx = polyfix(x, frx_dp, n, [0 2000], [0 0]);
+nr = 7;                                 % Degree of fitted polynomial
+fr = polyfix(xr, fr_dp, nr, [0 2000], [0 0]);
 
-plot(x,frx_dp, 'o'); hold on; grid on;
-plot(linspace(0,2000,2001), polyval(frx, linspace(0,2000,2001)));
-xlabel('Fish in the sea'); ylabel('Increase of fish per year');
-legend('Raw data', '7th degree polynomial fit');
+% plot(xr,fr_dp, 'o'); hold on; grid on;
+% plot(linspace(0,2000,2001), polyval(fr, linspace(0,2000,2001)));
+% xlabel('Fish in the sea'); ylabel('Increase of fish per year');
+% legend('Raw data', '7th degree polynomial fit');
+
+% ================ b. ================
+xe = [0 11 22 33 44 55 100]/100*2000;   % [0, 220, 440, 660, 880, 1100, 2000]
+fe_dp = [0, 10, 16, 20, 22, 23, 25];
+
+ne = 4;                                 % Degree of fitted polynomial
+fe = polyfix(xe, fe_dp, ne, [0 2000], [0 25]);
+
+% plot(xe,fe_dp, 'o'); hold on; grid on;
+% plot(linspace(0,2000,2001), polyval(fe, linspace(0,2000,2001)));
+% xlabel('Fish in the sea'); ylabel('Increase of fish per year');
+% legend('Raw data', '7th degree polynomial fit');
